c353305b2a03b831781bc47e62df7e9ef08a8e28
wgarciamunoz <88210706+wgarciamunoz@users.noreply.github.com>
2025-07-25 17:13:57 -0500

    The project has been updated to use the SDK style format targeting **.NET 8**. A package reference to `System.Drawing.Common` is included to maintain compatibility with existing image processing code.

--- ErrorCode.cs
+++ ErrorCode.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: Cyotek.GhostScript.ErrorCode
-// Assembly: Cyotek.GhostScript.PdfConversion, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: F846869A-603F-4B14-A759-B0D0C54D84D2
-// Assembly location: D:\Proyectos\Matika\ModelosGX\MatikaErp_Up\CSharpModel\web\bin\Cyotek.GhostScript.PdfConversion.dll
-
-#nullable disable
+﻿
 namespace Cyotek.GhostScript;
 
 public enum ErrorCode
